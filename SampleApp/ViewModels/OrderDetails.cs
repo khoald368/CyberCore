f5181ade2fdaf12771278d29bcb2a943bbeb234e
KHOALD <khoa.le@viettechsmart.com>
2022-02-18 14:58:15 +0700

    Update

---
+++ SampleApp/ViewModels/OrderDetails.cs
@@ -0,0 +1,17 @@
+﻿namespace SampleApp.ViewModels
+{
+    public class OrderDetails
+    {
+        public int Id { get; set; }
+
+        public int OrderId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int Ordinal { get; set; }
+    }
+}
---
+++ SampleApp/ViewModels/PaymentMethods.cs
@@ -0,0 +1,11 @@
+﻿namespace SampleApp.ViewModels
+{
+    public class PaymentMethods
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Status { get; set; }
+    }
+}
