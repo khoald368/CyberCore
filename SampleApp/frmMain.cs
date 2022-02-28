215ac89cfe30738f1ebb1a8ffa26af537e13f74d
KHOALD <khoa.le@viettechsmart.com>
2022-02-28 14:15:14 +0700

    Update

--- SampleApp/frmMain.cs
+++ SampleApp/frmMain.cs
@@ -13,6 +13,8 @@ namespace SampleApp
 {
     public partial class frmMain : Form
     {
+        private string conn = "Data Source=SQL5108.site4now.net;Initial Catalog=db_a7d12c_base;User Id=db_a7d12c_base_admin;Password=aassdd11;TrustServerCertificate=True;";
+
         public frmMain()
         {
             InitializeComponent();
@@ -20,7 +22,6 @@ namespace SampleApp
 
         private async void btnTest_Click(object sender, EventArgs e)
         {
-            var conn = "Data Source=SQL5108.site4now.net;Initial Catalog=db_a7d12c_base;User Id=db_a7d12c_base_admin;Password=aassdd11;TrustServerCertificate=True;";
             ISqlClient sql = new SqlClient();
             var z = await sql.QuerySingleOrDefaultAsync<string>(conn, "select top 1 fullname from customers;");
             txtInput.Text = z.ToString();
